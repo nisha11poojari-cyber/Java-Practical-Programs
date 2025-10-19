f3fc79b3ec7cfd88f03029bb80cb29f9aadcea89
nisha11poojari-cyber <nisha11poojari@gmail.com>
2025-10-19 07:43:37 +0530

    Add factorial calculation program
    This program calculates the factorial of a number provided as a command line argument, handling invalid inputs appropriately.

---
+++ Assignment 1/DemoFacorial.java
@@ -0,0 +1,28 @@
+/*Program to find the factorial of a number */
+
+class FactorialDemo {
+    public static void main(String[] args)
+    {
+        //Checking the number of parameters first
+        int n = args.length;
+        if(n != 1) {
+            System.out.println("\nInvalid number of arguments");
+            return;
+        }
+        //Converting the entered number which is a string(Command line argument) to an Integer
+        int num = Integer.parseInt(args[0]);
+        int fact = 1;
+        //Negative numbers don't have factorial
+        if(num < 0) {
+            System.out.println("\nInvalid input");
+            return;
+        }
+        //Works for zero as well
+        else {
+            for(int i=1;i<=num;i++) {
+                fact *= i;
+            }
+            System.out.println("\nFactorial = " + fact);
+        }
+    }
+}
